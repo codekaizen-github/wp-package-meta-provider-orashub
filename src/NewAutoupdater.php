228d99fce9e3d04f64288cea8e90fd32556370fe
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-08-18 12:32:09 +0000

    Add plugin and theme update parsers

--- src/NewAutoupdater.php
+++ src/NewAutoupdater.php
@@ -1,4 +1,8 @@
 <?php
+
+use Respect\Validation\Validator;
+use Respect\Validation\Rules;
+
 interface PreSetSiteTransientUpdateHookProviderInterface
 {
     public function getLocalPackageSlug(): string;
@@ -96,8 +100,8 @@ interface PackageMetaForDetailsProviderInterface extends PackageMetaForUpdateChe
     /** @return string */
     public function getRequiresPlugins(): array;
 }
-interface PackageMetaForDetailsThemeProviderInterface extends PackageMetaForDetailsProviderInterface {}
-interface PackageMetaForDetailsPluginProviderInterface extends PackageMetaForDetailsProviderInterface
+interface PackageMetaForDetailsProviderThemeInterface extends PackageMetaForDetailsProviderInterface {}
+interface PackageMetaForDetailsProviderPluginInterface extends PackageMetaForDetailsProviderInterface
 {
     public function getPluginFile(): string;
     public function getSections(): array;
@@ -141,8 +145,8 @@ class PreSetSiteTransientUpdateHookProviderV1 implements PreSetSiteTransientUpda
 class FormatMetaForTransientProviderPluginV1 implements FormatMetaForTransientProviderInterface
 {
     private PackageMetaForUpdateCheckProviderInterface $localPackageMetaProvider;
-    private PackageMetaForDetailsPluginProviderInterface $remotePackageMetaProvider;
-    public function __construct(PackageMetaForUpdateCheckProviderInterface $localPackageMetaProvider, PackageMetaForDetailsPluginProviderInterface $remotePackageMetaProvider)
+    private PackageMetaForDetailsProviderPluginInterface $remotePackageMetaProvider;
+    public function __construct(PackageMetaForUpdateCheckProviderInterface $localPackageMetaProvider, PackageMetaForDetailsProviderPluginInterface $remotePackageMetaProvider)
     {
         $this->localPackageMetaProvider = $localPackageMetaProvider;
         $this->remotePackageMetaProvider = $remotePackageMetaProvider;
@@ -160,8 +164,8 @@ class FormatMetaForTransientProviderPluginV1 implements FormatMetaForTransientPr
 class FormatMetaForTransientProviderThemeV1 implements FormatMetaForTransientProviderInterface
 {
     private PackageMetaForUpdateCheckProviderInterface $localPackageMetaProvider;
-    private PackageMetaForDetailsThemeProviderInterface $remotePackageMetaProvider;
-    public function __construct(PackageMetaForUpdateCheckProviderInterface $localPackageMetaProvider, PackageMetaForDetailsThemeProviderInterface $remotePackageMetaProvider)
+    private PackageMetaForDetailsProviderThemeInterface $remotePackageMetaProvider;
+    public function __construct(PackageMetaForUpdateCheckProviderInterface $localPackageMetaProvider, PackageMetaForDetailsProviderThemeInterface $remotePackageMetaProvider)
     {
         $this->localPackageMetaProvider = $localPackageMetaProvider;
         $this->remotePackageMetaProvider = $remotePackageMetaProvider;
@@ -174,5 +178,154 @@ class FormatMetaForTransientProviderThemeV1 implements FormatMetaForTransientPro
         $metaObject->package = $this->remotePackageMetaProvider->getDownloadURL();
         $metaObject->url = $this->remotePackageMetaProvider->getViewURL();
         return $metaObject;
+    }
+}
+class PackageMetaForUpdateCheckProviderPlugin implements PackageMetaForUpdateCheckProviderInterface
+{
+    protected string $filePath;
+    protected string $fullSlug;
+    protected string $shortSlug;
+    /**
+     * @param string $filePath Path to the plugin file
+     * @throws \InvalidArgumentException If the file path is invalid
+     */
+    public function __construct(string $filePath)
+    {
+        if (!file_exists($filePath) || !is_readable($filePath)) {
+            throw new \InvalidArgumentException("Invalid or inaccessible file path: $filePath");
+        }
+        $this->filePath = $filePath;
+        $this->fullSlug = plugin_basename($this->filePath);
+        // Get the last segment after any slash
+        $lastSegment = basename($this->fullSlug);
+        // Remove extension (if any) to get just the filename
+        $this->shortSlug = pathinfo($lastSegment, PATHINFO_FILENAME);
+    }
+    /**
+     * @return string
+     * @throws Respect\Validation\Exceptions\ValidationException If the package version number does not exist or is not semver.
+     */
+    public function getVersion(): string
+    {
+        $packageData = $this->getPackageData();
+        $value = $packageData['Version'] ?? null;
+        Validator::create(new Rules\StringType(), new Rules\Version())->check($packageData['Version'] ?? null);
+        // After validation, we can assert the type
+        /** @var string $value Type is guaranteed to be string after validation */
+        return $value;
+    }
+    /**
+     * Slug including any prefix - may contain a "/".
+     *
+     * @return string
+     */
+    public function getFullSlug(): string
+    {
+        return $this->fullSlug;
+    }
+    /**
+     * Slug minus any prefix. Should not contain a "/".
+     *
+     * @return string
+     */
+    public function getShortSlug(): string
+    {
+        return $this->shortSlug;
+    }
+    protected function getPackageData()
+    {
+        if (! function_exists('get_plugin_data')) {
+            require_once ABSPATH . 'wp-admin/includes/plugin.php';
+        }
+        return get_plugin_data($this->filePath, false, false);
+    }
+}
+class PackageMetaForUpdateCheckProviderTheme implements PackageMetaForUpdateCheckProviderInterface
+{
+    protected string $filePath;
+    protected string $fullSlug;
+    protected string $shortSlug;
+    /**
+     * @param string $filePath Path to the plugin file
+     * @throws \InvalidArgumentException If the file path is invalid
+     */
+    public function __construct(string $filePath)
+    {
+        if (!file_exists($filePath) || !is_readable($filePath)) {
+            throw new \InvalidArgumentException("Invalid or inaccessible file path: $filePath");
+        }
+        $this->filePath = $filePath;
+        $this->fullSlug = basename(dirname($this->filePath));
+        $this->shortSlug = $this->fullSlug;
+    }
+    /**
+     * @return string
+     * @throws Respect\Validation\Exceptions\ValidationException If the package version number does not exist or is not semver.
+     */
+    public function getVersion(): string
+    {
+        $packageData = $this->getPackageData();
+        $value = $packageData['Version'] ?? null;
+        Validator::create(new Rules\StringType(), new Rules\Version())->check($packageData['Version'] ?? null);
+        // After validation, we can assert the type
+        /** @var string $value Type is guaranteed to be string after validation */
+        return $value;
+    }
+    /**
+     * Slug including any prefix - may contain a "/".
+     *
+     * @return string
+     */
+    public function getFullSlug(): string
+    {
+        return $this->fullSlug;
+    }
+    /**
+     * Slug minus any prefix. Should not contain a "/".
+     *
+     * @return string
+     */
+    public function getShortSlug(): string
+    {
+        return $this->shortSlug;
+    }
+    protected function getPackageData()
+    {
+        if (! function_exists('wp_get_theme')) {
+            require_once ABSPATH . 'wp-includes/theme.php';
+        }
+        $theme_data = wp_get_theme($this->fullSlug);
+
+        // Define all possible theme headers
+        $headers = array(
+            'Name' => 'Theme Name',
+            'ThemeURI' => 'Theme URI',
+            'Author' => 'Author',
+            'AuthorURI' => 'Author URI',
+            'Description' => 'Description',
+            'Version' => 'Version',
+            'RequiresWP' => 'Requires at least',
+            'TestedWP' => 'Tested up to',
+            'RequiresPHP' => 'Requires PHP',
+            'License' => 'License',
+            'LicenseURI' => 'License URI',
+            'TextDomain' => 'Text Domain',
+            'Tags' => 'Tags',
+            'DomainPath' => 'Domain Path',
+            'UpdateURI' => 'Update URI',
+            'Template' => 'Template',
+            'Status' => 'Status'
+        );
+
+        // Convert WP_Theme object to array for consistency
+        $result = array();
+        foreach ($headers as $key => $header) {
+            $value = $theme_data->get($key);
+            if ($value) {
+                $result[$key] = $value;
+            }
+        }
+
+        return $result;
     }
 }
