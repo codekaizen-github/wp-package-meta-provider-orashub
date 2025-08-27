3d117049a9d05fc2efe6009583b190fa5d7f381f
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-08-27 12:11:39 +0000

    Add Theme validator

--- src/Client/ORASHub/Validation/Rule/ORASHubPackageMetaRuleTheme.php
+++ src/Client/ORASHub/Validation/Rule/ORASHubPackageMetaRuleTheme.php
@@ -14,8 +14,40 @@ class ORASHubPackageMetaRuleTheme extends Simple
             return false;
         }
 
-        return Validator::create(new Rules\Attribute('version', new Rules\Version(), true))
-            ->create(new Rules\Attribute('shortSlug', new Rules\StringType(), true))
-            ->create(new Rules\Attribute('fullSlug', new Rules\StringType(), true))->isValid($input);
+        return Validator::create(new Rules\Attribute('shortSlug', new Rules\StringType(), true))
+            ->create(new Rules\Attribute('fullSlug', new Rules\StringType(), true))
+            ->create(new Rules\Attribute('version', new Rules\Version(), true))
+            ->create(new Rules\Attribute('downloadURL', new Rules\Url(), true))
+            ->create(new Rules\Attribute('name', new Rules\StringType(), false))
+            ->create(new Rules\Attribute('viewURL', new Rules\Url(), false))
+            ->create(new Rules\Attribute('tested', new Rules\Version(), false))
+            ->create(new Rules\Attribute('stable', new Rules\Version(), false))
+            ->create(new Rules\Attribute(
+                'tags',
+                new Rules\AllOf(
+                    new Rules\ArrayType(),
+                    new Rules\Each(new Rules\StringType)
+                ),
+                false
+            ))
+            ->create(new Rules\Attribute('author', new Rules\StringType(), false))
+            ->create(new Rules\Attribute('authorURL', new Rules\Url(), false))
+            ->create(new Rules\Attribute('license', new Rules\StringType(), false))
+            ->create(new Rules\Attribute('licenseURL', new Rules\Url(), false))
+            ->create(new Rules\Attribute('shortDescription', new Rules\StringType(), false))
+            ->create(new Rules\Attribute('description', new Rules\StringType(), false))
+            ->create(new Rules\Attribute('requiresWordPressVersion', new Rules\Version(), false))
+            ->create(new Rules\Attribute('requiresPHPVersion', new Rules\Version(), false))
+            ->create(new Rules\Attribute('textDomain', new Rules\StringType(), false))
+            ->create(new Rules\Attribute('domainPath', new Rules\StringType(), false))
+            ->create(new Rules\Attribute(
+                'requiresPlugins',
+                new Rules\AllOf(
+                    new Rules\ArrayType(),
+                    new Rules\Each(new Rules\StringType)
+                ),
+                false
+            ))
+            ->isValid($input);
     }
 }
