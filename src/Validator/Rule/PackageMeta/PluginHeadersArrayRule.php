0d7fe4a75bcfa3a37e90a97c4c45aac860d55587
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-09-05 12:10:50 +0000

    Use Flexible Semantic Version Rule

--- src/Validator/Rule/PackageMeta/PluginHeadersArrayRule.php
+++ src/Validator/Rule/PackageMeta/PluginHeadersArrayRule.php
@@ -2,6 +2,7 @@
 
 namespace CodeKaizen\WPPackageMetaProviderLocal\Validator\Rule\PackageMeta;
 
+use CodeKaizen\WPPackageMetaProviderLocal\Validator\Rule\Version\FlexibleSemanticVersionRule;
 use Respect\Validation\Validator;
 use Respect\Validation\Rules;
 use Respect\Validation\Rules\Core\Simple;
@@ -17,15 +18,15 @@ class PluginHeadersArrayRule extends Simple
         return Validator::create(new Rules\AllOf(
             new Rules\Key('Name', new Rules\StringType(), true),
             new Rules\Key('PluginURI', new Rules\Url(), false),
-            new Rules\Key('Version', new Rules\Version(), false),
+            new Rules\Key('Version', new FlexibleSemanticVersionRule(), false),
             new Rules\Key('Description', new Rules\StringType(), false),
             new Rules\Key('Author', new Rules\StringType(), false),
             new Rules\Key('AuthorURI', new Rules\StringType(), false),
             new Rules\Key('TextDomain', new Rules\StringType(), false),
             new Rules\Key('DomainPath', new Rules\StringType(), false),
             new Rules\Key('Network', new Rules\BoolVal(), false),
-            new Rules\Key('RequiresWP', new Rules\Version(), false),
-            new Rules\Key('RequiresPHP', new Rules\Version(), false),
+            new Rules\Key('RequiresWP', new FlexibleSemanticVersionRule(), false),
+            new Rules\Key('RequiresPHP', new FlexibleSemanticVersionRule(), false),
             new Rules\Key('UpdateURI', new Rules\Url(), false),
             new Rules\Key('RequiresPlugins', new Rules\StringType(), false),
         ))->isValid($input);
