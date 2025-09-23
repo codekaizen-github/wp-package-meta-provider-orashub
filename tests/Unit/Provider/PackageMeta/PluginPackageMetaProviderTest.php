0e7ab895453758e8134a059cbaaa73b4dfee3ecd
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-09-23 12:06:30 +0000

    Test all properties of PluginPackageMetaProvider

--- tests/Unit/Provider/PackageMeta/PluginPackageMetaProviderTest.php
+++ tests/Unit/Provider/PackageMeta/PluginPackageMetaProviderTest.php
@@ -27,38 +27,81 @@ class PluginPackageMetaProviderTest extends TestCase {
 	 *
 	 * @return void
 	 */
-	public function testGetNameFromPluginMyBasicsPlugin(): void {
-		$response                  = [
-			'name'                     => 'Test Plugin',
-			'fullSlug'                 => 'test-plugin/test-plugin.php',
-			'shortSlug'                => 'test-plugin',
-			'version'                  => '3.0.1',
-			'viewUrl'                  => 'https://codekaizen.net',
-			'downloadUrl'              => 'https://codekaizen.net',
-			'tested'                   => '6.8.2',
-			'stable'                   => '6.8.2',
-			'tags'                     => [ 'tag1', 'tag2', 'tag3' ],
-			'author'                   => 'Andrew Dawes',
-			'authorUrl'                => 'https://codekaizen.net/team/andrew-dawes',
-			'license'                  => 'GPL v2 or later',
-			'licenseUrl'               => 'https://www.gnu.org/licenses/gpl-2.0.html',
-			'description'              => 'This is a test plugin',
-			'shortDescription'         => 'Test',
-			'requiresWordPressVersion' => '6.8.2',
-			'requiresPHPVersion'       => '8.2.1',
-			'textDomain'               => 'test-plugin',
-			'domainPath'               => '/languages',
-			'requiresPlugins'          => [ 'akismet', 'hello-dolly' ],
-			'sections'                 => [
-				'changelog' => 'changed',
-				'about'     => 'this is a plugin about section',
-			],
-			'network'                  => true,
+	public function testAllPropertiesFromPluginMyBasicsPlugin(): void {
+		$nameExpected                     = 'Test Plugin';
+		$fullSlugExpected                 = 'test-plugin/test-plugin.php';
+		$shortSlugExpected                = 'test-plugin';
+		$viewURLExpected                  = 'https://codekaizen.net';
+		$versionExpected                  = '3.0.1';
+		$shortDescriptionExpected         = 'This is a test plugin';
+		$authorExpected                   = 'Andrew Dawes';
+		$authorURLExpected                = 'https://codekaizen.net/team/andrew-dawes';
+		$textDomainExpected               = 'test-plugin';
+		$domainPathExpected               = '/languages';
+		$networkExpected                  = true;
+		$requiresWordPressVersionExpected = '6.8.2';
+		$requiresPHPVersionExpected       = '8.2.1';
+		$downloadURLExpected              = 'https://github.com/codekaizen-github/wp-package-meta-provider-local';
+		$testedExpected                   = '6.8.2';
+		$stableExpected                   = '6.8.2';
+		$requiresPluginsExpected          = [ 'akismet', 'hello-dolly' ];
+		$tagsExpected                     = [ 'tag1', 'tag2', 'tag3' ];
+		$licenseExpected                  = 'GPL v2 or later';
+		$licenseURLExpected               = 'https://www.gnu.org/licenses/gpl-2.0.html';
+		$descriptionExpected              = 'This is a long description of the plugin';
+		$sectionsExpected                 = [
+			'changelog' => 'changed',
+			'about'     => 'this is a plugin about section',
 		];
-		$metaAnnotationKeyAccessor = Mockery::mock( AssociativeArrayStringToMixedAccessorContract::class );
+		$response                         = [
+			'name'                     => $nameExpected,
+			'fullSlug'                 => $fullSlugExpected,
+			'shortSlug'                => $shortSlugExpected,
+			'version'                  => $versionExpected,
+			'viewUrl'                  => $viewURLExpected,
+			'downloadUrl'              => $downloadURLExpected,
+			'tested'                   => $testedExpected,
+			'stable'                   => $stableExpected,
+			'tags'                     => $tagsExpected,
+			'author'                   => $authorExpected,
+			'authorUrl'                => $authorURLExpected,
+			'license'                  => $licenseExpected,
+			'licenseUrl'               => $licenseURLExpected,
+			'description'              => $descriptionExpected,
+			'shortDescription'         => $shortDescriptionExpected,
+			'requiresWordPressVersion' => $requiresWordPressVersionExpected,
+			'requiresPHPVersion'       => $requiresPHPVersionExpected,
+			'textDomain'               => $textDomainExpected,
+			'domainPath'               => $domainPathExpected,
+			'requiresPlugins'          => $requiresPluginsExpected,
+			'sections'                 => $sectionsExpected,
+			'network'                  => $networkExpected,
+		];
+		$metaAnnotationKeyAccessor        = Mockery::mock( AssociativeArrayStringToMixedAccessorContract::class );
 		$metaAnnotationKeyAccessor->shouldReceive( 'get' )->with()->andReturn( $response );
 		$provider = new PluginPackageMetaProvider( $metaAnnotationKeyAccessor );
-		$this->assertEquals( 'Test Plugin', $provider->getName() );
+		$this->assertEquals( $nameExpected, $provider->getName() );
+		$this->assertEquals( $fullSlugExpected, $provider->getFullSlug() );
+		$this->assertEquals( $shortSlugExpected, $provider->getShortSlug() );
+		$this->assertEquals( $viewURLExpected, $provider->getViewURL() );
+		$this->assertEquals( $versionExpected, $provider->getVersion() );
+		$this->assertEquals( $shortDescriptionExpected, $provider->getShortDescription() );
+		$this->assertEquals( $authorExpected, $provider->getAuthor() );
+		$this->assertEquals( $authorURLExpected, $provider->getAuthorURL() );
+		$this->assertEquals( $textDomainExpected, $provider->getTextDomain() );
+		$this->assertEquals( $domainPathExpected, $provider->getDomainPath() );
+		$this->assertEquals( $networkExpected, $provider->getNetwork() );
+		$this->assertEquals( $requiresWordPressVersionExpected, $provider->getRequiresWordPressVersion() );
+		$this->assertEquals( $requiresPHPVersionExpected, $provider->getRequiresPHPVersion() );
+		$this->assertEquals( $downloadURLExpected, $provider->getDownloadURL() );
+		$this->assertEquals( $requiresPluginsExpected, $provider->getRequiresPlugins() );
+		$this->assertEquals( $testedExpected, $provider->getTested() );
+		$this->assertEquals( $stableExpected, $provider->getStable() );
+		$this->assertEquals( $licenseExpected, $provider->getLicense() );
+		$this->assertEquals( $licenseURLExpected, $provider->getLicenseURL() );
+		$this->assertEquals( $descriptionExpected, $provider->getDescription() );
+		$this->assertEquals( $tagsExpected, $provider->getTags() );
+		$this->assertEquals( $sectionsExpected, $provider->getSections() );
 	}
 	/**
 	 * Test
@@ -66,44 +109,106 @@ class PluginPackageMetaProviderTest extends TestCase {
 	 * @return void
 	 */
 	public function testJSONEncodeAndDecode(): void {
-
-		$response                  = [
-			'name'                     => 'Test Plugin',
-			'fullSlug'                 => 'test-plugin/test-plugin.php',
-			'shortSlug'                => 'test-plugin',
-			'version'                  => '3.0.1',
-			'viewUrl'                  => 'https://codekaizen.net',
-			'downloadUrl'              => 'https://codekaizen.net',
-			'tested'                   => '6.8.2',
-			'stable'                   => '6.8.2',
-			'tags'                     => [ 'tag1', 'tag2', 'tag3' ],
-			'author'                   => 'Andrew Dawes',
-			'authorUrl'                => 'https://codekaizen.net/team/andrew-dawes',
-			'license'                  => 'GPL v2 or later',
-			'licenseUrl'               => 'https://www.gnu.org/licenses/gpl-2.0.html',
-			'description'              => 'This is a test plugin',
-			'shortDescription'         => 'Test',
-			'requiresWordPressVersion' => '6.8.2',
-			'requiresPHPVersion'       => '8.2.1',
-			'textDomain'               => 'test-plugin',
-			'domainPath'               => '/languages',
-			'requiresPlugins'          => [ 'akismet', 'hello-dolly' ],
-			'sections'                 => [
-				'changelog' => 'changed',
-				'about'     => 'this is a plugin about section',
-			],
-			'network'                  => true,
+		$nameExpected                     = 'Test Plugin';
+		$fullSlugExpected                 = 'test-plugin/test-plugin.php';
+		$shortSlugExpected                = 'test-plugin';
+		$viewURLExpected                  = 'https://codekaizen.net';
+		$versionExpected                  = '3.0.1';
+		$shortDescriptionExpected         = 'This is a test plugin';
+		$authorExpected                   = 'Andrew Dawes';
+		$authorURLExpected                = 'https://codekaizen.net/team/andrew-dawes';
+		$textDomainExpected               = 'test-plugin';
+		$domainPathExpected               = '/languages';
+		$networkExpected                  = true;
+		$requiresWordPressVersionExpected = '6.8.2';
+		$requiresPHPVersionExpected       = '8.2.1';
+		$downloadURLExpected              = 'https://github.com/codekaizen-github/wp-package-meta-provider-local';
+		$testedExpected                   = '6.8.2';
+		$stableExpected                   = '6.8.2';
+		$requiresPluginsExpected          = [ 'akismet', 'hello-dolly' ];
+		$tagsExpected                     = [ 'tag1', 'tag2', 'tag3' ];
+		$licenseExpected                  = 'GPL v2 or later';
+		$licenseURLExpected               = 'https://www.gnu.org/licenses/gpl-2.0.html';
+		$descriptionExpected              = 'This is a long description of the plugin';
+		$sectionsExpected                 = [
+			'changelog' => 'changed',
+			'about'     => 'this is a plugin about section',
 		];
-		$metaAnnotationKeyAccessor = Mockery::mock( AssociativeArrayStringToMixedAccessorContract::class );
-		$metaAnnotationKeyAccessor->shouldReceive( 'get' )->with()->andReturn( $response );
-		$provider = new PluginPackageMetaProvider( $metaAnnotationKeyAccessor );
-		$this->assertEquals( 'Test Plugin', $provider->getName() );
+		$response                         = [
+			'name'                     => $nameExpected,
+			'fullSlug'                 => $fullSlugExpected,
+			'shortSlug'                => $shortSlugExpected,
+			'version'                  => $versionExpected,
+			'viewUrl'                  => $viewURLExpected,
+			'downloadUrl'              => $downloadURLExpected,
+			'tested'                   => $testedExpected,
+			'stable'                   => $stableExpected,
+			'tags'                     => $tagsExpected,
+			'author'                   => $authorExpected,
+			'authorUrl'                => $authorURLExpected,
+			'license'                  => $licenseExpected,
+			'licenseUrl'               => $licenseURLExpected,
+			'description'              => $descriptionExpected,
+			'shortDescription'         => $shortDescriptionExpected,
+			'requiresWordPressVersion' => $requiresWordPressVersionExpected,
+			'requiresPHPVersion'       => $requiresPHPVersionExpected,
+			'textDomain'               => $textDomainExpected,
+			'domainPath'               => $domainPathExpected,
+			'requiresPlugins'          => $requiresPluginsExpected,
+			'sections'                 => $sectionsExpected,
+			'network'                  => $networkExpected,
+		];
+		$client                           = Mockery::mock( AssociativeArrayStringToMixedAccessorContract::class );
+		$client->shouldReceive( 'get' )->with()->andReturn( $response );
+		$provider = new PluginPackageMetaProvider( $client );
 		// phpcs:ignore WordPress.WP.AlternativeFunctions.json_encode_json_encode
 		$encoded = json_encode( $provider );
 		$this->assertIsString( $encoded );
 		$decoded = json_decode( $encoded, true );
 		$this->assertIsArray( $decoded );
 		$this->assertArrayHasKey( 'name', $decoded );
-		$this->assertEquals( 'Test Plugin', $decoded['name'] );
+		$this->assertEquals( $nameExpected, $decoded['name'] );
+		$this->assertArrayHasKey( 'fullSlug', $decoded );
+		$this->assertEquals( $fullSlugExpected, $decoded['fullSlug'] );
+		$this->assertArrayHasKey( 'shortSlug', $decoded );
+		$this->assertEquals( $shortSlugExpected, $decoded['shortSlug'] );
+		$this->assertArrayHasKey( 'viewUrl', $decoded );
+		$this->assertEquals( $viewURLExpected, $decoded['viewUrl'] );
+		$this->assertArrayHasKey( 'version', $decoded );
+		$this->assertEquals( $versionExpected, $decoded['version'] );
+		$this->assertArrayHasKey( 'shortDescription', $decoded );
+		$this->assertEquals( $shortDescriptionExpected, $decoded['shortDescription'] );
+		$this->assertArrayHasKey( 'author', $decoded );
+		$this->assertEquals( $authorExpected, $decoded['author'] );
+		$this->assertArrayHasKey( 'authorUrl', $decoded );
+		$this->assertEquals( $authorURLExpected, $decoded['authorUrl'] );
+		$this->assertArrayHasKey( 'textDomain', $decoded );
+		$this->assertEquals( $textDomainExpected, $decoded['textDomain'] );
+		$this->assertArrayHasKey( 'domainPath', $decoded );
+		$this->assertEquals( $domainPathExpected, $decoded['domainPath'] );
+		$this->assertArrayHasKey( 'network', $decoded );
+		$this->assertEquals( $networkExpected, $decoded['network'] );
+		$this->assertArrayHasKey( 'requiresWordPressVersion', $decoded );
+		$this->assertEquals( $requiresWordPressVersionExpected, $decoded['requiresWordPressVersion'] );
+		$this->assertArrayHasKey( 'requiresPHPVersion', $decoded );
+		$this->assertEquals( $requiresPHPVersionExpected, $decoded['requiresPHPVersion'] );
+		$this->assertArrayHasKey( 'downloadUrl', $decoded );
+		$this->assertEquals( $downloadURLExpected, $decoded['downloadUrl'] );
+		$this->assertArrayHasKey( 'requiresPlugins', $decoded );
+		$this->assertEquals( $requiresPluginsExpected, $decoded['requiresPlugins'] );
+		$this->assertArrayHasKey( 'tested', $decoded );
+		$this->assertEquals( $testedExpected, $decoded['tested'] );
+		$this->assertArrayHasKey( 'stable', $decoded );
+		$this->assertEquals( $stableExpected, $decoded['stable'] );
+		$this->assertArrayHasKey( 'license', $decoded );
+		$this->assertEquals( $licenseExpected, $decoded['license'] );
+		$this->assertArrayHasKey( 'licenseUrl', $decoded );
+		$this->assertEquals( $licenseURLExpected, $decoded['licenseUrl'] );
+		$this->assertArrayHasKey( 'description', $decoded );
+		$this->assertEquals( $descriptionExpected, $decoded['description'] );
+		$this->assertArrayHasKey( 'tags', $decoded );
+		$this->assertEquals( $tagsExpected, $decoded['tags'] );
+		$this->assertArrayHasKey( 'sections', $decoded );
+		$this->assertEquals( $sectionsExpected, $decoded['sections'] );
 	}
 }
