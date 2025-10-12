337d421ff45f409929410655adcf05ea02b3fb3d
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-10-12 17:06:59 +0000

    Test Factories

---
+++ tests/Unit/Factory/Provider/PackageMeta/ThemePackageMetaProviderFactoryV1Test.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Test
+ *
+ * @package CodeKaizen\WPPackageMetaProviderORASHubTests\Unit\Factory\Provider\PackageMeta
+ */
+
+namespace CodeKaizen\WPPackageMetaProviderORASHubTests\Unit\Factory\Provider\PackageMeta;
+
+use CodeKaizen\WPPackageMetaProviderORASHub\Factory\Provider\PackageMeta\ThemePackageMetaProviderFactoryV1;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Undocumented class
+ */
+class ThemePackageMetaProviderFactoryV1Test extends TestCase {
+	/**
+	 * Undocumented function
+	 *
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 * @return void
+	 */
+	public function testFactoryCreatesProvider(): void {
+		Mockery::mock(
+			'overload:CodeKaizen\WPPackageMetaProviderORASHub\Provider\PackageMeta\ThemePackageMetaProvider',
+			'CodeKaizen\WPPackageMetaProviderContract\Contract\ThemePackageMetaContract'
+		);
+		$url               = 'http://example.com';
+		$metaAnnotationKey = 'org.example.meta';
+		$logger            = Mockery::mock( LoggerInterface::class );
+		$sut               = new ThemePackageMetaProviderFactoryV1(
+			$url,
+			$metaAnnotationKey,
+			$logger
+		);
+		$return            = $sut->create();
+		$this->assertInstanceOf(
+			'CodeKaizen\WPPackageMetaProviderContract\Contract\ThemePackageMetaContract',
+			$return
+		);
+	}
+}
